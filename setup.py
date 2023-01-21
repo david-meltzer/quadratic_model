3effe49916050ae647783dd817428c7322231fca
david-meltzer <76928255+david-meltzer@users.noreply.github.com>
2023-01-21 18:46:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 
 # read the contents of your README file
-#from os import path
-#this_directory = path.abspath(path.dirname(__file__))
-#with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-#    long_description = f.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
     
 #INSTALL_REQUIRES = [
 #    'jax>=0.1.77',
 #]
 
 setup(
-    name='quadratic_model',
+    name='Quadratic Model',
     license='MIT License',
+    author='David Meltzer',
     author_email='davidhmeltzer@gmail.com',
     install_requires=INSTALL_REQUIRES,
     url='https://github.com/david-meltzer/quadratic_model',
